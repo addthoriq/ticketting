73dc0a9a22d3971609e92a277b73f53ad39e349d
addthoriq <alvarro.thoriq@gmail.com>
2021-01-15 21:00:31 +0800

    lastt

--- index.php
+++ index.php
@@ -14,26 +14,62 @@ if (!isset($_SESSION['id'])) {
         <?php
             include 'config/header.php';
         ?>
-        <h1>Hai kamu</h1>
+        <h1 style="text-align: center; color: #d92139">Selamat datang di Sistem Penjualan Tiket Pesawat.</h1>
+        <p style="text-align: center; color: #d92139">Silahkan klik menu Pemesanan untuk melakukan pemesanan tiket pesawat</p>
     </body>
 </html>
 <!-- Sudah Login -->
 <?php
 }else {
-?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Tiket Pesawat</title>
-        <link rel="stylesheet" href="gaya.css">
-    </head>
-    <body>
-        <?php
-            include 'config/header.php';
-        ?>
-    </body>
-</html>
-<?php
+    function gate($n){
+        include 'config/koneksi.php';
+
+        $sql = "SELECT * from pegawai WHERE nama_pegawai = '$n'";
+        $qry = mysqli_query($koneksi, $sql);
+        $row = mysqli_fetch_assoc($qry);
+        if (isset($row['nama_pegawai']) == $n) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    // Cek Pegawai
+    if (gate($_SESSION['nama'])) {
+    ?>
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+            <head>
+                <meta charset="utf-8">
+                <title>Tiket Pesawat</title>
+                <link rel="stylesheet" href="gaya.css">
+            </head>
+            <body>
+                <?php
+                    include 'config/header.php';
+                ?>
+                <h1 style="text-align: center; color: #d92139">Selamat datang di Sistem Penjualan Tiket Pesawat.</h1>
+                <p style="text-align: center; color: #d92139">Silahkan klik menu Laporan untuk melihat laporan transaksi pemesanan tiket pesawat</p>
+            </body>
+        </html>
+    <?php
+    }else {
+    ?>
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+            <head>
+                <meta charset="utf-8">
+                <title>Tiket Pesawat</title>
+                <link rel="stylesheet" href="gaya.css">
+            </head>
+            <body>
+                <?php
+                    include 'config/header.php';
+                ?>
+                <h1 style="text-align: center; color: #d92139">Selamat datang di Sistem Penjualan Tiket Pesawat.</h1>
+                <p style="text-align: center; color: #d92139">Silahkan klik menu Pemesanan untuk melakukan pemesanan tiket pesawat</p>
+            </body>
+        </html>
+    <?php
+    }
 }
 ?>
